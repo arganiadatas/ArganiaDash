c09c881865c0ab481659a3eb7197d7127642ba54
ARGANIA! <135002378+arganiadatas@users.noreply.github.com>
2025-10-29 20:50:43 -0300

    Update usd.ts

--- server/data/usd.ts
+++ server/data/usd.ts
@@ -1,6 +1,11 @@
 import type { CurrencyQuote } from "@shared/schema";
 
 export const usdHistory: CurrencyQuote[] = [
+  { time: "2025-10-15", value: 40.25},
+  { time: "2025-10-16", value: 40.21},
+  { time: "2025-10-17", value: 40.17},
+  { time: "2025-10-18", value: 40.16},
+  { time: "2025-10-19", value: 40.19},
   { time: "2025-10-23", value: 40.23 },
   { time: "2025-10-24", value: 40.25 },
   { time: "2025-10-25", value: 40.20 },
